3ae77f9928f71bc508c5c71ccba1cd34cea99e84
Benjamin Winkler <abwinkler999@gmail.com>
2013-11-03 17:05:07 -0500

    Restructured tests

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,5 +1,36 @@
 class RomanNumeral
+	def pre_and_post_parser(numeral, lxv)
+		
+	end
+
 	def r_to_i(numeral)
-		return numeral.length
+
+		if numeral.empty?
+			return nil
+		end
+
+		numeral.strip!
+		numeral.upcase!
+
+		if numeral.include? "L"
+			return 50
+		end
+
+		if numeral.include? "X"
+			return 10
+		end
+
+		if numeral.include? "V"
+			#if numeral.partition("V")
+			if numeral.include? "IV"
+				return 4
+			elsif numeral.include? "VI"
+				return 6
+			else
+				return 5
+			end
+		else
+			return numeral.length
+		end
 	end
 end
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -2,8 +2,18 @@ require 'roman_numeral'
 
 describe RomanNumeral do
 
+	it "refuses to parse strings that are not Roman numerals" do
+		subject.r_to_i("").should == nil
+		subject.r_to_i("FOO").should == nil
+	end
+
 	it "can parse I into 1" do
 		subject.r_to_i("I").should == 1
+	end
+
+	it "can clean up poorly formatted arguments" do
+		subject.r_to_i("i").should == 1
+		subject.r_to_i(" i \n").should == 1
 	end
 
 	it "can parse II into 2" do
@@ -16,5 +26,17 @@ describe RomanNumeral do
 
 	it "can parse IV into 4" do
 		subject.r_to_i("IV").should == 4
+	end
+
+	it "can parse V into 5" do
+		subject.r_to_i("V").should == 5
+	end
+
+	it "can parse X into 10" do
+		subject.r_to_i("X").should == 10
+	end
+
+	it "can parse L into 50" do
+		subject.r_to_i("L").should == 50
 	end
 end
